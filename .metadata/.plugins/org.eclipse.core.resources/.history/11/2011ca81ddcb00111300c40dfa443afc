d6382b1bd2805bffae3e5c322ffb4e82c53a6538
Sean Zhang <sean@localhost.localdomain>
2012-07-11 22:01:26 -0700

    Initial commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/11/2011ca81ddcb00111300c40dfa443afc
@@ -0,0 +1,17 @@
+package Ch2;
+
+
+public class Ch2_4 {
+	/**
+	 * Assume Matrix is a square matrix
+	 * @param m
+	 */
+	public void rotate_square_matrix_cw(Matrix m) {
+		if (m.getRowSize() != m.getColumnSize()) {
+			throw new IllegalArgumentException(); 
+		}
+		
+		// Do a four-way swap
+		
+	}
+}
---
+++ CareerCup3rdEdition/src/Ch2/Matrix.java
@@ -0,0 +1,17 @@
+package Ch2;
+
+import java.util.List;
+
+/**
+ * 
+ * @author sean
+ *
+ */
+public interface Matrix {
+	public Double get(final int i, final int j);
+	public void set(final int i, final int j, final double value);
+	public List<Integer> getSize();
+	public int getRowSize();
+	public int getColumnSize();
+	public String toString();
+}
