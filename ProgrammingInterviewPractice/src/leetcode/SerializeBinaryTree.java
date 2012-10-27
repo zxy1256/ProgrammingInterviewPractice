b32a6b75cd76d605e95e2387842abb6aaabee064
Sean <sean@sean-laptop-linux.(none)>
2012-10-26 23:26:08 -0700

    Implemented the solution in leetcode. Much simpler.

--- ProgrammingInterviewPractice/src/leetcode/SerializeBinaryTree.java
+++ ProgrammingInterviewPractice/src/leetcode/SerializeBinaryTree.java
@@ -1,85 +1,46 @@
 package leetcode;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * http://www.leetcode.com/2010/09/serializationdeserialization-of-binary.html
  */
 public class SerializeBinaryTree {
     public String writeBinaryTree(Node root) {
         if (root == null) {
-            return "[]";
+            return "#";
         }
 
-        String result = "[" + String.valueOf(root.value);
+        String result = String.valueOf(root.value);
         result = result + "," + writeBinaryTree(root.left);
         result = result + "," + writeBinaryTree(root.right);
-        result = result + "]";
         return result;
     }
 
     public Node readBinaryTree(String s) {
-        return stringToBinaryTree(s.toCharArray(), 0, s.length() - 1);
+        List<String> tokens = Arrays.asList(s.split(","));
+        Iterator<String> itr = tokens.iterator();
+        return readBinaryTree(itr);
     }
 
-    private Node stringToBinaryTree(char[] s, int begin, int end) {
-        if (begin >= end) {
-            throw new IllegalArgumentException();
-        }
-
-        if (end - begin == 1) {
+    private Node readBinaryTree(Iterator<String> itr) {
+        if (!itr.hasNext()) {
             return null;
         }
 
-        assert (end - begin >= 8); // Not null, then minimum case is [1,[],[]]
-        int valueLeft = begin + 1;
-        int valueRight = valueLeft;
-        while (s[valueRight] != ',' && valueRight < s.length) {
-            valueRight++;
+        String token = itr.next();
+        if (token.equals("#")) {
+            return null;
         }
 
-        int value = charToValue(s, valueLeft, valueRight);
+        int value = Integer.parseInt(token);
         Node root = new Node(value, null, null);
-        int leftBracketOfLeftTree = valueRight + 1;
-        assert s[leftBracketOfLeftTree] == '[';
-        int rightBracketOfLeftTree = getMatchBracket(s, leftBracketOfLeftTree);
-        int leftBracketOfRightTree = rightBracketOfLeftTree + 2;
-        assert s[leftBracketOfRightTree] == '[';
-        int rightBracketOfRightTree = getMatchBracket(s, leftBracketOfRightTree);
-
-        root.left = stringToBinaryTree(s, leftBracketOfLeftTree,
-                rightBracketOfLeftTree);
-        root.right = stringToBinaryTree(s, leftBracketOfRightTree,
-                rightBracketOfRightTree);
+        root.left = readBinaryTree(itr);
+        root.right = readBinaryTree(itr);
 
         return root;
-    }
-
-    private int getMatchBracket(char[] s, int left) {
-        assert s[left] == '[';
-        int bracketCount = 1;
-        int current = left + 1;
-        while (bracketCount > 0 && current < s.length) {
-            if (s[current] == '[') {
-                bracketCount++;
-            }
-
-            if (s[current] == ']') {
-                bracketCount--;
-            }
-
-            current++;
-        }
-        assert s[current - 1] == ']';
-        return current - 1;
-    }
-
-    private int charToValue(char[] s, int begin, int end) {
-        assert begin <= end;
-        assert end <= s.length;
-        StringBuilder strBuilder = new StringBuilder();
-        for (int i = begin; i < end; i++) {
-            strBuilder.append(s[i]);
-        }
-        return Integer.parseInt(strBuilder.toString());
     }
 
     public static class Node {
